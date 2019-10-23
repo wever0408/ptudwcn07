162a986fe8aa22548e7c2b1b989a811f0840c410
wever0408 <39722379+wever0408@users.noreply.github.com>
2019-10-23 20:48:48 +0700

    add action for authentication

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -9,12 +9,40 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-  
+  axios
+    .post("/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
 
 // Login - get user token
 export const loginUser = userData => dispatch => {
- 
+  axios
+    .post("/users/login", userData)
+    .then(res => {
+      // Save to localStorage
+
+      // Set token to localStorage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
 
 // Set logged in user
@@ -34,5 +62,10 @@ export const setUserLoading = () => {
 
 // Log user out
 export const logoutUser = () => dispatch => {
-  
+  // Remove token from local storage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to empty object {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
 };
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Board from '../components/Board';
-import * as action from '../actions';
+import * as action from '../actions/gameActions';
 
 const nSquareToWin = 5;
 function calculateWinner(squares) {
